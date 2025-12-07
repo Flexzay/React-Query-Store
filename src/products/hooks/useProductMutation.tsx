ae3f9938163ea041440a80db163772bfdaea8c84
Ricardo Rivera <cristianricardoriveramontanez@gmail.com>
2025-12-06 23:55:24 -0500

    setqueryData

--- src/products/hooks/useProductMutation.tsx
+++ src/products/hooks/useProductMutation.tsx
@@ -1,25 +1,31 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createProduct } from "../services/actions";
+import { Product } from "../interface/Product";
 
 export const useProductMutation = () => {
-
-const queryClient = useQueryClient()
+  const queryClient = useQueryClient();
 
   const productMutation = useMutation({
     mutationFn: createProduct,
     onSuccess: (data) => {
       console.log("Producto creado ");
 
+      // queryClient.invalidateQueries(
+      //     ['products', data.category]
+      // )
 
-        queryClient.invalidateQueries(
-            ['products', data.category]
-        )
-
+      queryClient.setQueryData<Product[] | undefined>(
+        ["products", data.category],
+        (old) => {
+          if (!old) return [data];
+          return [...old, data];
+        }
+      );
     },
     onSettled: () => {
       console.log("on settle ");
     },
   });
-  
+
   return productMutation;
 };
